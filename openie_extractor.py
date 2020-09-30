be5dcf3bf0a2d6345ea164407aa2c830be9444eb
mannem srinivas <m.srinivas365@gmail.com>
2020-10-01 01:13:00 +0530

    Whole code has been replaced
    Stanza works better than third party tool

--- openie_extractor.py
+++ openie_extractor.py
@@ -1,16 +1,22 @@
-from openie import StanfordOpenIE
 import json
+from stanza.server import CoreNLPClient
 
-with StanfordOpenIE() as client:
-    # text = "Shea butter is an excellent moisturizer for the face and the body. Its fat content is responsible for its emollient and humectant properties. It locks in the moisture in the skin and keeps it hydrated for long. Dehydrated and dry skin becomes rough and scaly. Certain areas of the body can even develop skin cracks due to dryness. Shea butter can nourish the skin with its fat content. It can also help to soften the skin on your hands and feet and make it supple. It penetrates the skin easily, without clogging the pores, and is effective for dry skin. Use shea butter to heal cracked heels, dry cuticles, and rough patches on your skin. You can also use it to simply moisturize your skin during the colder months."
-    # print('Text: %s.' % text)
-    # for triple in client.annotate(text):
-    #     print('|-', triple)
 
-    with open("data.json") as json_data:
-        data=json.load(json_data)
-        for cat,text in zip(data.keys(),data.values()):
-            print(cat)
-            print(text)
-            for triple in client.annotate(text):
-                print('|-', triple)
+text = "Shea butter is an excellent moisturizer for the face and the body. Its fat content is responsible for its emollient and humectant properties. It locks in the moisture in the skin and keeps it hydrated for long. Dehydrated and dry skin becomes rough and scaly. Certain areas of the body can even develop skin cracks due to dryness. Shea butter can nourish the skin with its fat content. It can also help to soften the skin on your hands and feet and make it supple. It penetrates the skin easily, without clogging the pores, and is effective for dry skin. Use shea butter to heal cracked heels, dry cuticles, and rough patches on your skin. You can also use it to simply moisturize your skin during the colder months."
+with CoreNLPClient(
+        annotators=['openie'],
+        timeout=30000,
+        memory='16G') as client:
+    core_nlp_output = client.annotate(text,output_format='json')
+    f=open("core_nlp_output.json","w")
+    f.write(json.dumps(core_nlp_output))
+    f.close()
+    triples=[]
+    for sentence in core_nlp_output['sentences']:
+        for triple in sentence['openie']:
+            triples.append({
+                'subject': triple['subject'],
+                'relation': triple['relation'],
+                'object': triple['object']
+            })
+    print(triples)
